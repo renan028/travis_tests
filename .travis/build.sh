78d45aad979ed6c7df503132d13d4321a51ee040
renan028 <renan028@gmail.com>
2021-05-27 06:20:54 -0300

    add head~

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,8 +7,14 @@ echo "Building $TRAVIS_BRANCH"
 CHANGED_FILES="$(git diff --name-only ${TRAVIS_BRANCH}..HEAD --)"
 echo ${CHANGED_FILES}
 
+echo "HEAD@{0} HEAD@{1}: "
 CHANGED_FILES="$(git diff --name-only HEAD@{0} HEAD@{1})"
 echo ${CHANGED_FILES}
 
+echo "TRAVIS_COMMIT_RANGE: "
 CHANGED_FILES="$(git diff --name-only $TRAVIS_COMMIT_RANGE)"
+echo ${CHANGED_FILES}
+
+echo "HEAD~..HEAD: "
+CHANGED_FILES="$(git diff --name-only HEAD~..HEAD)"
 echo ${CHANGED_FILES}
